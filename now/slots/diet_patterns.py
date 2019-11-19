ce35389bf69749c3eef2cde8756f4959ec1bc2ec
Sergio Romano <only.romano@gmail.com>
2019-11-19 15:22:00 +0600

    slots diet cascade update

--- now/slots/diet_patterns.py
+++ now/slots/diet_patterns.py
@@ -1,12 +1,11 @@
 try:
     from my_values import my_values_preset as MVP
 except ImportError:
-    from diet_patterns import get_MVP
     # put your values here
-    height =  180               # your height in sm (inches * 2.54)
-    age = 25                    # your age
-    gender = "M"                # i'm sorry but to correct formula gender needed (available options: "M" for male, "F" for female, "T" for transgender)
-    muscle = False              # a lot of muscles?
+    height =  None              # your height in sm (inches * 2.54)
+    age = None                  # your age
+    gender = None               # to correct formula gender needed (available options: "M" for male, "F" for female, "T" for transgender)
+    muscle = None               # a lot of muscles?
     desirable_weight = None     # leave None to auto-calculate else input float
     MVP = get_MVP(height, age, gender, muscle, desirable_weight)
 
@@ -41,14 +40,92 @@ def DIET(slots):
     else:
         return DP.STAGE_ANOREXY(slots)
     """
-    return "ДИЕТА"
+    return _diet_text(_stage_constructor())
 
 
-def get_MVP(height, age, gender, muscle, desirable_weight=None):
+def get_MVP(height, age, gender, muscle, d_weight=None):
     return {
-        "weight": None,
-        "muscle": None,
-        "fat": None,
-        "water": None,
-        "IMT": None,
+        "weight": 66.6,
+        "muscle": 27.5,
+        "fat": 15.0,
+        "water": 50.0,
+        "IMT": (d_weight / height**2 if d_weight and height else 22.5),
         }
+
+
+# internal use functions
+def _diet_text(stage):
+    return "\t\tДиета на сегодня. " + "\n\t\t\t".join([
+    "СТАДИЯ № %i\n" % stage['number'],
+    stage['sport'],
+    stage['description'] + "\n",
+    "ПОКАЗАТЕЛИ:     %.1f - %.1f кГ" % (stage['weight_min'], stage['weight_max']),
+    "РАСЧЁТН. ВРЕМЯ: %s ... (%s) ... %s" % (stage['date_min'], stage['date_med'], stage['date_max']),
+    "%s" % ("\n\t\t\t" + stage['swing'] + "\n" if stage['swing'] else ""),
+    _get_element_value("БЕЛОК:", stage['protein']),
+    _get_element_value("САХАР:", stage['carbonate']),
+    _get_element_value("ЖИРЫ: ", stage['fat']) + "\n",
+    "ККАЛ:     %i - %i ККал" % (stage['calories']['min'], stage['calories']['max'])
+    ])
+
+
+def _stage_constructor():
+    return {
+        "number": 0,
+        "sport": "sport",
+        "description": "description",
+        "weight_min": 0.0,
+        "weight_max": 0.0,
+        "date_min": "01 jan 1970",
+        "date_med": "19 jan 2020",
+        "date_max": "20 mar 2020",
+        "swing": "",
+        "calories": {
+            "min": 0,
+            "max": 0,
+            },
+        "protein": {
+            "mass": {
+                "min": 0,
+                "max": 0,
+                },
+            "per_kg": {
+                "min": 0,
+                "max": 0,
+                },
+            "procent": 0.0,
+            },
+        "carbonate": {
+            "mass": {
+                "min": 0,
+                "max": 0,
+                },
+            "per_kg": {
+                "min": 0,
+                "max": 0,
+                },
+            "procent": 0.0,
+            },
+        "fat": {
+            "mass": {
+                "min": 0,
+                "max": 0,
+                },
+            "per_kg": {
+                "min": 0,
+                "max": 0,
+                },
+            "procent": 0.0,
+            },
+        }
+
+
+def _get_element_value(label, values):
+    return "%s    %i - %i гр   (~ %i - %i мг/кГ)  -  [%.1f %%]" % (
+            label,
+            values['mass']['min'],
+            values['mass']['max'],
+            values['per_kg']['min'],
+            values['per_kg']['max'],
+            values['procent']
+        )
