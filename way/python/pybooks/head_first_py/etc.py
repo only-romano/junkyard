9868a27a7e6b03c3c0ccffd2bd8e7ce669d90d44
Sergio Romano <only.romano@gmail.com>
2020-09-13 11:47:29 +0600

    python way head first book update

---
+++ way/python/pybooks/head_first_py/etc.py
@@ -0,0 +1,9 @@
+# Various stuff from the book
+
+# diy 1
+movies = ["The Holy Grail", "The Life of Brian", "The Meaning of Life"]
+for i in range(3):
+    movies.insert(1 + i*2, 1975 + i*4)
+
+print(movies)
+
